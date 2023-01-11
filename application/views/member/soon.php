73c724bcc5b6d788cfabe52d3c0441af4cc51bbe
Adi Purnama <imadeadipurnamayasa@gmail.com>
2023-01-11 16:31:38 +0800

    Update #1

---
+++ application/views/member/soon.php
@@ -0,0 +1,16 @@
+<div class="d-flex justify-content-center">
+    <div class="col-12 col-lg-8 col-xl-6">
+        <div class="container" style="margin-bottom: 8rem;">
+            <div class="app-container py-5">
+                <?php $this->load->view("member/header"); ?>
+                <div class="row d-flex justify-content-center">
+                    <div class="col-12">
+                        <div class="receive-title d-flex flex-row justify-content-center align-items-center mb-4">
+                            <span class="ms-2">COMING SOON!</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
